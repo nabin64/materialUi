f235e19d8ed42fb7b2fd9ca4b501fb90b5d3830e
nabin <lamanabin64@gmail.com>
2023-06-24 12:20:47 +0545

    first commit

---
+++ src/pages/Create.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Typography from '@mui/material/Typography';
+
+
+export default function Create() {
+  return (
+    <div>
+
+     <Typography variant='h6' component="h2" gutterBottom color="secondary">
+      Crate a New Note
+     </Typography>
+    </div>
+  )
+}
